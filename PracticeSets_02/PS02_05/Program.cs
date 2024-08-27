1a3fe397ab209e3a071a5bf388a86be6ca1ccede
Lawrence <lawsmagnet6@gmail.com>
2024-08-28 00:03:33 +0800

    msg

---
+++ PracticeSets_02/PS02_05/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS02_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            *Write a program that takes a string as input and outputs the frequency of each character in the string. 
+            */
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (frequency.ContainsKey(c))
+                {
+                    frequency[c]++;
+                }
+                else
+                {
+                    frequency[c] = 1;
+                }
+            }
+
+            Console.WriteLine("Character frequencies:");
+            foreach (var pair in frequency)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+    }
+}
---
+++ PracticeSets_02/PS02_07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS02_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            *Write a program that takes an array of numbers as input and outputs the sum of the numbers. 
+            */
+            Console.WriteLine("Enter numbers separated by spaces:");
+
+            
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+
+            int sum = 0;
+
+            foreach (string number in numbers)
+            {
+                
+                if (int.TryParse(number, out int num))
+                {
+                    sum += num;
+                }
+                
+            }
+
+            Console.WriteLine($"The sum of the numbers is: {sum}");
+        }
+    }
+}
