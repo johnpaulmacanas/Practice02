5a5cde83060f663247f349920b718282bbf150b7
johnpaulmacanas <202380256@psu.palawan.edu.ph>
2024-08-27 23:40:11 +0800

    your message

---
+++ PracticeSets_02/PS02_04/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace PS02_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * Write a program that takes a string as input and outputs whether it is a palindrome. 
+            */
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            if (IsPalindrome(input))
+            {
+                Console.WriteLine("The string is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The string is not a palindrome.");
+            }
+        }
+
+        static bool IsPalindrome(string s)
+        {
+            string cleaned = Regex.Replace(s, "[^a-zA-Z0-9]", "").ToLower();
+
+            return cleaned == Reverse(cleaned);
+        }
+
+        static string Reverse(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+
+}
---
+++ PracticeSets_02/PS02_06/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS02_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * Write a program that takes a number as input and outputs the sum of its digits. 
+            */
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            if (int.TryParse(input, out int number))
+            {
+                number = Math.Abs(number);
+
+                for (int temp = number; temp > 0; temp /= 10)
+                {
+                    int digit = temp % 10;
+                    sum += digit;
+                }
+
+                Console.WriteLine("The sum of the digits is: " + sum);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+
+}
